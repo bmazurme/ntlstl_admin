64a0569db9fdd0f64b242af9e6efb332d14e4a22
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-06-27 22:51:39 +0300

    feat: mui

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,11 @@ import ErrorBoundaryWrapper from './components/error-boundary';
 import App from './app';
 import { store } from './store';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
