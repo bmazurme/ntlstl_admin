07ffb71b893cebeb0e313b6488c01d9d23574ba9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-06-29 12:21:51 +0300

    fix: constants

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,13 +1,13 @@
-const TYPE = {
-  BLOCK: 'BLOCK',
-  ITEM: 'ITEM',
-};
+// const TYPE = {
+//   BLOCK: 'BLOCK',
+//   ITEM: 'ITEM',
+// };
 
-const COLOR = {
-  BLOCK: 'rgba(150,252,92,0.8)',
-  ITEM: 'rgba(243,224,205,0.8)',
-  VALUE: 'yellow',
-};
+// const COLOR = {
+//   BLOCK: 'rgba(150,252,92,0.8)',
+//   ITEM: 'rgba(243,224,205,0.8)',
+//   VALUE: 'yellow',
+// };
 
 const Urls = {
   BASE: {
@@ -15,22 +15,6 @@ const Urls = {
     PROJECT: {
       BOOK: ':projectId/:bookId',
     },
-  },
-  PROJECTS: {
-    INDEX: '/projects',
-    ID: ':projectId',
-    BOOK_ID: ':projectId/:bookId',
-  },
-  CHART: {
-    INDEX: '/chart',
-  },
-  PASSWORD: {
-    FORGOT: '/password/forgot',
-    CONFIRM: '/password/:token',
-    RESET: '/password',
-  },
-  PROFILE: {
-    INDEX: '/profile',
   },
   OAUTH: {
     INDEX: 'oauth',
@@ -43,16 +27,10 @@ const Urls = {
     YANDEX: 'https://oauth.yandex.ru/authorize?response_type=code&client_id=c709762dfe3e447999beb343da0bee9f',
     GITHUB: 'https://github.com/login/oauth/authorize?client_id=d3a7b43f6b10622202cc',
   },
-  SUPPORT: {
-    INDEX: '/support',
-  },
-  USERS: {
-    INDEX: '/users',
-  },
   KIT: {
     INDEX: '/kit',
   },
   404: '*',
 };
 
-export { TYPE, COLOR, Urls };
+export { Urls };
