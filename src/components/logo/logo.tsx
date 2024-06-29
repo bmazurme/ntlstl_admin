11c72297a739ca09b5906094433d4817fabd314c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-06-29 12:19:42 +0300

    fix: logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+
+import { Button } from '@mui/material';
+import { BlurOn as BlurOnIcon } from '@mui/icons-material';
 
 export default function Logo() {
+  const navigate = useNavigate();
+  const onHandleMove = (url: string) => navigate(url);
+
   return (
-    <div>
-      ntlstl.admin
-    </div>
+    <Button
+      variant="text"
+      startIcon={<BlurOnIcon />}
+      onClick={() => onHandleMove('/')}
+    >
+      [ntlstl.admin]
+    </Button>
   );
 }
