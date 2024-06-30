a63e73189631ecb0ae728284894313f2b69dfe3a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-06-30 11:10:18 +0300

    fix: main

--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
-import Container from '../../components/container';
+import Content from '../../components/content';
 import Main from '../../components/main';
-
-import style from './main-layout.module.css';
 
 export default function MainLayout() {
   return (
-    <div className={style.app}>
-      <Container children={<Main />} />
-    </div>
+    <Content header children={<Main />} />
   );
 }
