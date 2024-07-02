b6c38eb8d8466ba1c2f4021192328fdb3adbfdb7
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-02 15:51:14 +0300

    fix: logo.tsx

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -7,12 +7,16 @@ import { BlurOn as BlurOnIcon } from '@mui/icons-material';
 export default function Logo() {
   const navigate = useNavigate();
   const onHandleMove = (url: string) => navigate(url);
+  const onHandleRefresh = () => {
+    onHandleMove('/');
+    window.location.reload();
+  };
 
   return (
     <Button
       variant="text"
       startIcon={<BlurOnIcon />}
-      onClick={() => onHandleMove('/')}
+      onClick={onHandleRefresh}
     >
       [ntlstl.admin]
     </Button>
