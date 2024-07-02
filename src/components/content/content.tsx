ea8dae6e7a270fd4c66bdfd686623a9ca2b1e9dc
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-07-02 15:48:28 +0300

    fix: content.tsx

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import type { PropsWithChildren } from 'react';
 
+import { Box } from '@mui/material';
+
 import Header from '../header';
 import Footer from '../footer';
-
-import style from './content.module.css';
 
 export default function Content({ children, header, footer }
   : PropsWithChildren & { header?: boolean; footer?: boolean; }) {
   return (
-    <main className={style.content}>
+    <Box component="main">
       {header && <Header />}
       {children}
       {footer && <Footer />}
-    </main>
+    </Box>
   );
 }
